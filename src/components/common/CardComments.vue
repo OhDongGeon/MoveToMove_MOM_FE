90b76507ad181dce8fe1e0ea26da8a8ce8eacb09
백재우 <9712jw@gmail.com>
2024-09-09 15:22:18 +0900

    change/bjw/코멘트 데이터 연결

--- src/components/common/CardComments.vue
+++ src/components/common/CardComments.vue
@@ -21,28 +21,18 @@
 </template>
 
 <script>
+import { useCommentStore } from '@/stores/commentStore';
+import { computed } from 'vue';
+
 export default {
   name: 'CardComments',
 
   setup() {
     const isAdmin = true;
+    const commentStore = useCommentStore();
 
-    const comments = [
-      {
-        id: 1,
-        author: '파이어리',
-        content: '칸반 보드는 작업을 시작하고, 진행 중인 작업을 계획하여 효율을 최대화하는 애자일 프로젝트 관리 도구입니다.',
-        date: '2024-08-20 13:30',
-        avatar: 'https://over-clock-s3.s3.ap-northeast-2.amazonaws.com//img/5e56fa91-f87d-4f4b-86f0-c46d5cbaace4.png',
-      },
-      {
-        id: 2,
-        author: '팬텀',
-        content: '워크플로우 시작과 관련된 설명이 추가될 예정입니다.',
-        date: '2024-08-19 10:20',
-        avatar: 'https://over-clock-s3.s3.ap-northeast-2.amazonaws.com//img/60409475-953c-4658-8fb4-7807c0c379a0.jpg',
-      },
-    ];
+    // 스토어에서 코멘트 목록을 가져오기
+    const comments = computed(() => commentStore.getComments || null );
 
     return {
       comments,
