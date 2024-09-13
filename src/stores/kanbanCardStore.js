cbedf94136baecf5df11f0feb87636bf47a0bcae
badstar1004 <dmswjd4566@hanmail.net>
2024-09-13 18:23:24 +0900

    Modify: 카드 스토어 수정
    - 업데이트 함수가 없었음

--- src/stores/kanbanCardStore.js
+++ src/stores/kanbanCardStore.js
@@ -3,85 +3,99 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useKanbanCardStore = defineStore('kanbanCard', () => {
-    const cards = ref([]);
+  const cards = ref([]);
 
-    const loadAllCards = async (projectId) => {
-        try {
-            const response = await axiosInstance.get(`/api/projects/${projectId}/kanban-cards`); //
-            cards.value = response.data.map((card) => ({
-                id: card.kanbanCardId,
-                columnId: card.kanbanColumnId,
-                title: card.title,
-                priority: card.priority,
-                task_size: card.taskSize,
-                members: [],
-            }));
-        } catch (error) {
-            console.error('Failed to load cards:', error);
+  const loadAllCards = async (projectId) => {
+    try {
+      const response = await axiosInstance.get(`/api/projects/${projectId}/kanban-cards`); //
+      cards.value = response.data.map((card) => ({
+        id: card.kanbanCardId,
+        columnId: card.kanbanColumnId,
+        title: card.title,
+        priority: card.priority,
+        task_size: card.taskSize,
+        members: [],
+      }));
+    } catch (error) {
+      console.error('Failed to load cards:', error);
+    }
+  };
+
+  const loadCardDetails = async (cardId) => {
+    try {
+      const response = await axiosInstance.get(`/api/kanban-cards/${cardId}/details`);
+      const cardData = response.data;
+      return {
+        id: cardData.kanbanColumnInCard.kanbanCardId,
+        columnId: cardData.kanbanColumnInCard.kanbanColumnId,
+        columnName: cardData.kanbanColumnInCard.kanbanColumnName,
+        title: cardData.kanbanColumnInCard.title,
+        content: cardData.kanbanColumnInCard.content,
+        priority: cardData.kanbanColumnInCard.priority,
+        task_size: cardData.kanbanColumnInCard.taskSize,
+        startAt: cardData.kanbanColumnInCard.startAt,
+        endAt: cardData.kanbanColumnInCard.endAt,
+        createdAt: cardData.kanbanColumnInCard.createdAt,
+
+        members: cardData.cardMemberList.map((member) => ({
+          memberId: member.memberId,
+          nickName: member.nickName,
+          profileUrl: member.profileUrl,
+        })),
+
+        comments: cardData.cardCommentList || [],
+      };
+    } catch (error) {
+      console.error('Failed to load card details:', error);
+    }
+  };
+
+  const getCardsByColumnId = (columnId) => {
+    return cards.value.filter((card) => card.columnId === columnId);
+  };
+
+  // 칸반 컬럼 움직임에 따른 카드 움직임
+  const updateCardsForMovedColumn = (updatedColumns) => {
+    // 각 컬럼의 새로운 인덱스와 함께 카드 업데이트
+    updatedColumns.forEach((column) => {
+      cards.value.forEach((card) => {
+        if (card.columnId === column.id) {
+          // 카드의 columnId를 업데이트된 컬럼의 인덱스로 설정
+          card.columnId = column.id;
         }
-    };
+      });
+    });
+  };
 
-    const loadCardDetails = async (cardId) => {
-        try {
-            const response = await axiosInstance.get(`/api/kanban-cards/${cardId}/details`);
-            const cardData = response.data;
-            return {
-                id: cardData.kanbanColumnInCard.kanbanCardId,
-                columnId: cardData.kanbanColumnInCard.kanbanColumnId,
-                columnName: cardData.kanbanColumnInCard.kanbanColumnName,
-                title: cardData.kanbanColumnInCard.title,
-                content: cardData.kanbanColumnInCard.content,
-                priority: cardData.kanbanColumnInCard.priority,
-                task_size: cardData.kanbanColumnInCard.taskSize,
-                startAt: cardData.kanbanColumnInCard.startAt,
-                endAt: cardData.kanbanColumnInCard.endAt,
-                createdAt: cardData.kanbanColumnInCard.createdAt,
+  // 칸반 카드 제목 수정
+  const updateKanbanCardTitle = async (cardId, updateColumn, updateData) => {
+    try {
+      // const form = {
+      //     updateColumn: updateColumn,
+      //     updateData: updateData,
+      // };
 
-                members: cardData.cardMemberList.map((member) => ({
-                    memberId: member.memberId,
-                    nickName: member.nickName,
-                    profileUrl: member.profileUrl,
-                })),
+      // const response = await axiosInstance.patch(`/api/kanban-cards/${cardId}`, form);
 
-                comments: cardData.cardCommentList || [],
-            };
-        } catch (error) {
-            console.error('Failed to load card details:', error);
-        }
-    };
+      console.log(cardId, updateColumn, updateData);
 
-    const getCardsByColumnId = (columnId) => {
-        return cards.value.filter((card) => card.columnId === columnId);
-    };
+      const index = cards.value.findIndex((card) => card.id === cardId);
+      console.log(cards.value[index]);
 
-    const updateKanbanCardTitle = async (cardId, updateColumn, updateData) => {
-        try {
-            // const form = {
-            //     updateColumn: updateColumn,
-            //     updateData: updateData,
-            // };
+      console.log(index);
 
-            // const response = await axiosInstance.patch(`/api/kanban-cards/${cardId}`, form);
+      // if (index !== -1) {
+      //     cards.value[index] = {
+      //         ...cards.value[index], // 기존 카드 데이터 유지
+      //         ...(cards[index][updateColumn] = updateData),
+      //     };
+      // }
 
-            console.log(cardId, updateColumn, updateData);
+      console.log('변경', cards.value[index]);
+    } catch (error) {
+      console.error('Failed to load card details:', error);
+    }
+  };
 
-            const index = cards.value.findIndex((card) => card.id === cardId);
-            console.log(cards.value[index]);
-
-            console.log(index);
-
-            // if (index !== -1) {
-            //     cards.value[index] = {
-            //         ...cards.value[index], // 기존 카드 데이터 유지
-            //         ...(cards[index][updateColumn] = updateData),
-            //     };
-            // }
-
-            console.log('변경', cards.value[index]);
-        } catch (error) {
-            console.error('Failed to load card details:', error);
-        }
-    };
-
-    return { cards, loadAllCards, loadCardDetails, getCardsByColumnId, updateKanbanCardTitle };
+  return { cards, loadAllCards, loadCardDetails, getCardsByColumnId, updateCardsForMovedColumn, updateKanbanCardTitle };
 });
